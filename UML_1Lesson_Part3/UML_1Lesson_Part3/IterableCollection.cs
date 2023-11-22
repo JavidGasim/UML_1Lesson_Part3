e11324472f8c0d19f8de831a51c22d29b1c4fa50
JavidGasim <143398895+JavidGasim@users.noreply.github.com>
2023-11-22 16:06:14 +0400

    Add files via upload

---
+++ UML_1Lesson_Part3/UML_1Lesson_Part3/IterableCollection.cs
@@ -0,0 +1,14 @@
+﻿namespace UML_1Lesson_Part3;
+
+public interface IterableCollection
+{
+    public Iterator createIterator();
+}
+
+public class ConcreteCollection : IterableCollection
+{
+    public Iterator createIterator()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ UML_1Lesson_Part3/UML_1Lesson_Part3/Iterator.cs
@@ -0,0 +1,27 @@
+﻿namespace UML_1Lesson_Part3;
+
+public interface Iterator
+{
+    public void getNext();
+    public bool hasMore();
+}
+
+public class ConcreteIterator : Iterator
+{
+    private ConcreteCollection collection;
+    private object iterationState;
+
+    public ConcreteIterator(ConcreteCollection c)
+    {
+        collection = c;
+    }
+    public void getNext()
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool hasMore()
+    {
+        throw new NotImplementedException();
+    }
+}
